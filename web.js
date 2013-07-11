2fdb598c438c309321e67097a5a97a98f054b6be
Hannah Slodki <hslodki@gmail.com>
2013-07-11 06:04:51 +0000

    Add single quotes around filename

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
-// var express = require('express');
+var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(fs.readFileSync(index.html), "utf-8"); 
+var buffer = new Buffer(fs.readFileSync('index.html'), "utf-8");
 
 app.get('/', function(request, response) {
   response.send(buffer.toString());
